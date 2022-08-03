b17898e5fe1b72445250f44ce37c51c52898b691
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2022-08-03 20:31:29 +0300

    lab problem 2

--- AsynchronousProgramming/02.BLOG/app.js
+++ AsynchronousProgramming/02.BLOG/app.js
@@ -1,5 +1,54 @@
-function attachEvents() {
-    console.log('TODO...');
+const Selectors = {
+    posts: '#posts',
 }
 
-attachEvents();
+const url = 'https://blog-apps-c12bf.firebaseio.com/';
+
+let postId = '';
+
+const lastAddres = {
+    posts: 'posts',
+    post: 'posts/',
+    comments: 'comments',
+}
+const json = '.json';
+
+let posts;
+
+const postLoader = function () {
+    const dataHandler = function (data) {
+        posts = document.querySelector(Selectors.posts)
+        const fragment = new DocumentFragment();
+
+        for (const item in data) {
+            if (Object.hasOwnProperty.call(data, item)) {
+                const element = data[item];
+
+                const option = document.createElement('option')
+                option.value = item;
+                option.textContent = element.title;
+                fragment.appendChild(option);
+            }
+        }
+
+        posts.appendChild(fragment);
+    }
+
+    fetch(url + lastAddres.posts + json)
+        .then((response) => response.json())
+        .then(dataHandler);
+}
+
+const postViewer = function () {
+    postId = posts.options[posts.selectedIndex].value;
+
+    const dataHandler = function (data) {
+        for (const comment of data.comments) {
+            console.log(comment);
+        }
+    }
+
+    fetch(url + lastAddres.post + postId + json)
+        .then((response) => response.json())
+        .then(dataHandler);
+}
