16d8f995954679569a910abfa5086867649fe7fb
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2022-08-08 20:30:05 +0300

    Exercise

--- ExerciseAsynchronousProgramming/02. FISHER-GAME/app.js
+++ ExerciseAsynchronousProgramming/02. FISHER-GAME/app.js
@@ -1,6 +1,39 @@
-function attachEvents() {
-    console.log('TODO...');
+const catchesUrl = 'https://fisher-game.firebaseio.com/catches.json';
+
+const load = async () => {
+    const response = await fetch(loadUrl);
+    const catchers = await response.json();
+
+    for (const item in catchers) {
+        if (Object.hasOwnProperty.call(catchers, item)) {
+            const catcher = catchers[item];
+            console.log(catcher);
+        }
+    }
+};
+
+const update = () => {
+
+};
+
+const deleteCatcher = () => {
+
+};
+
+const add = async () => {
+    const angler = getHTMLElement('#addForm > input.angler');
+    const weight = getHTMLElement('#addForm > input.weight');
+    const species = getHTMLElement('#addForm > input.species');
+    const location = getHTMLElement('#addForm > input.location');
+    const bait = getHTMLElement('#addForm > input.bait');
+    const captureTime = getHTMLElement('#addForm > input.captureTime');
+    const catcher = new Catch(angler, weight, species, location, bait, captureTime);
+    const response = await fetch(catchesUrl, {
+        method: 'POST',
+        body: JSON.stringify(catcher),
+    });
+};
+
+const getHTMLElement = (selector) => {
+    return document.querySelector(selector);
 }
-
-attachEvents();
-
---
+++ ExerciseAsynchronousProgramming/02. FISHER-GAME/catch.js
@@ -0,0 +1,10 @@
+class Catch {
+    constructor(angler, weight, species, location, bait, captureTime) {
+        this.angler = angler;
+        this.weight = weight;
+        this.species = species;
+        this.location = location;
+        this.bait = bait;
+        this.captureTime = captureTime;
+    }
+}
