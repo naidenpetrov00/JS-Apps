23fdb32a32b8045bb955f49dc04cbc743de10ed4
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2022-08-19 20:20:03 +0300

    lab

--- RemoteDatabases/testApp.js
+++ RemoteDatabases/testApp.js
@@ -1,11 +1,15 @@
 const selectors = {
     bookNumber: '#bookNumber',
+    bookShield: '#books',
 }
 
 const errorHandling = {
     noNumberForBook: 'Please specify the number of the book you search',
     noHTMLElement: 'Invalid HTML element',
+    noSuchBook: 'No such book in library',
 }
+
+let bookShield = '';
 
 const searchBook = async () => {
     const number = getHTMLElement(selectors.bookNumber).value;
@@ -13,9 +17,24 @@ const searchBook = async () => {
         alert(errorHandling.noNumberForBook);
     }
 
-    const url = `https://testapp-6b674-default-rtdb.firebaseio.com/Books/${number}`;
+    const url = `https://testapp-6b674-default-rtdb.firebaseio.com/Books/${number}.json`;
 
+    const response = await fetch(url);
+    const book = await response.json();
 
+    if (book === null) {
+        alert(errorHandling.noSuchBook);
+    }
+    else {
+        bookShield = getHTMLElement(selectors.bookShield);
+        printBook(book);
+    }
+}
+
+const printBook = (book) => {
+    const li = createHTMLElement('li');
+    li.textContent = `author: ${book.author}, title: ${book.title}`;
+    bookShield.appendChild(li);
 }
 
 const getHTMLElement = (selector) => {
@@ -24,5 +43,11 @@ const getHTMLElement = (selector) => {
         throw new Error(errorHandling.noHTMLElement)
     }
 
-    return element
+    return element;
+}
+
+const createHTMLElement = (tagName) => {
+    const element = document.createElement(tagName);
+
+    return element;
 }
