c0aab1d72d373b5ad04d1365f748a0d07db64378
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2022-08-21 16:07:39 +0300

    lab

--- RemoteDatabases/testApp.js
+++ RemoteDatabases/testApp.js
@@ -1,5 +1,7 @@
 const selectors = {
     bookNumber: '#bookNumber',
+    bookAuthor: '#bookAuthor',
+    bookTitle: '#bookTitle',
     bookShield: '#books',
 }
 
@@ -7,28 +9,44 @@ const errorHandling = {
     noNumberForBook: 'Please specify the number of the book you search',
     noHTMLElement: 'Invalid HTML element',
     noSuchBook: 'No such book in library',
+    emptyDataForNewBook: 'Please specify valid author and title',
 }
 
-let bookShield = '';
+let bookShield;
 
 const searchBook = async () => {
     const number = getHTMLElement(selectors.bookNumber).value;
     if (number === '') {
         alert(errorHandling.noNumberForBook);
+        throw Error(errorHandling.noNumberForBook);
     }
 
     const url = `https://testapp-6b674-default-rtdb.firebaseio.com/Books/${number}.json`;
-
     const response = await fetch(url);
     const book = await response.json();
 
     if (book === null) {
         alert(errorHandling.noSuchBook);
+        throw Error(errorHandling.noSuchBook);
     }
-    else {
-        bookShield = getHTMLElement(selectors.bookShield);
-        printBook(book);
+
+    bookShield = getHTMLElement(selectors.bookShield);
+    printBook(book);
+}
+
+const addBook = async () => {
+    const author = getHTMLElement(selectors.bookAuthor).value;
+    const title = getHTMLElement(selectors.bookTitle).value;
+    if (author === '', title === '') {
+        alert(errorHandling.emptyDataForNewBook)
+        throw Error(errorHandling.emptyDataForNewBook);
     }
+
+    const book = {
+        title: title,
+        author: author,
+    };
+    
 }
 
 const printBook = (book) => {
