a5dc00e05e338a04135cea2df6196a0bc0b42d53
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2022-09-01 23:42:00 +0300

    lab

--- RoutingandArchitecture/Team-Manager-Skeleton/scripts/app.js
+++ RoutingandArchitecture/Team-Manager-Skeleton/scripts/app.js
@@ -1,29 +1,84 @@
-let logged = true;
-
 $(() => {
     const app = Sammy('#main', function () {
-        const homePageForLogedUser = () => {
-            this.swap('<h2>Home Page</h2>');
+        const homePage = () => {
+            document.getElementById('home').style.display = "block";
+            document.getElementById('catalog').style.display = "none";
+            document.getElementById('about').style.display = "none";
+            document.getElementById('login').style.display = "none";
+            document.getElementById('register').style.display = "none";
         };
 
-        const homePageForNotRegisteredUser = () => {
-            
-        };
+        const catalogPage = () => {
+            document.getElementById('about').style.display = "none";
+            document.getElementById('catalog').style.display = "block";
+            document.getElementById('home').style.display = "none";
+            document.getElementById('login').style.display = "none";
+            document.getElementById('register').style.display = "none";
+        }
 
-        this.get('#/index.html', () => {
-            if (logged) {
-                homePageForLogedUser();
-            }
-            else {
-                homePageForNotRegisteredUser;
-            }
-        });
+        const aboutPage = () => {
+            document.getElementById('about').style.display = "block";
+            document.getElementById('catalog').style.display = "none";
+            document.getElementById('home').style.display = "none";
+            document.getElementById('login').style.display = "none";
+            document.getElementById('register').style.display = "none";
+        }
 
-        this.get('#/about', () => {
-            this.swap('<h2>About Page</h2>');
-        });
+        const loginPage = () => {
+            document.getElementById('home').style.display = "none";
+            document.getElementById('catalog').style.display = "none";
+            document.getElementById('about').style.display = "none";
+            document.getElementById('login').style.display = "block";
+            document.getElementById('register').style.display = "none";
+        }
+
+        const registerPage = () => {
+            document.getElementById('home').style.display = "none";
+            document.getElementById('catalog').style.display = "none";
+            document.getElementById('about').style.display = "none";
+            document.getElementById('login').style.display = "none";
+            document.getElementById('register').style.display = "block";
+        }
+
+        this.get('#/index.html', homePage);
+        this.get('#/catalog', catalogPage);
+        this.get('#/about', aboutPage);
+        this.get('#/login', loginPage);
+        this.get('#/register', registerPage);
     });
 
     app.run();
 });
 
+const login = () => {
+    const username = document.querySelector('#login #username').value;
+    const password = document.querySelector('#login #password').value;
+};
+
+const register = async () => {
+    let username = document.querySelector('#register #username');
+    let password = document.querySelector('#register #password');
+    let repeatedPassword = document.querySelector('#register #repeatPassword');
+    const registerUrl = 'https://teammanager-3991a-default-rtdb.firebaseio.com/Users.json';
+
+    if (password.value !== repeatedPassword.value) {
+        alert('You should repeat the same password');
+    }
+    else {
+        const user = {
+            username,
+            password
+        };
+
+        const response = await fetch(registerUrl, {
+            method: "POST",
+            body: JSON.stringify(user),
+        });
+        
+        alert('user registered successfully');
+
+        username.value = '';
+        password.value = '';
+        repeatedPassword.value = '';
+    }
+};
